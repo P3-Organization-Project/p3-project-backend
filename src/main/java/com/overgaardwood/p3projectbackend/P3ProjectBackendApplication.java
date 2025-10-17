2da304e50ac153ac71a3ef21b9d12cd2a7e4c3d0
StagnantBytes <artyom.shevchenko2005@gmail.com>
2025-10-18 00:39:04 +0200

    starting the refactoring proccess

---
+++ src/main/java/com/overgaardwood/p3projectbackend/P3ProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.overgaardwood.p3projectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3ProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P3ProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/overgaardwood/p3projectbackend/P3ProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.overgaardwood.p3projectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3ProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
